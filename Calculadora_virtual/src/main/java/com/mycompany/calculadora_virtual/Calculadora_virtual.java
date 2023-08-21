1573df8cb54315e3e5edc06afac8411a822613f8
Carlos20daniel <1440648@senati.pe>
2023-08-21 12:24:24 -0500

    creacion de carpeta

---
+++ Calculadora_virtual/src/main/java/com/mycompany/calculadora_virtual/Calculadora_virtual.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadora_virtual;
+
+/**
+ *
+ * @author Carlos Cuenca
+ */
+public class Calculadora_virtual {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
