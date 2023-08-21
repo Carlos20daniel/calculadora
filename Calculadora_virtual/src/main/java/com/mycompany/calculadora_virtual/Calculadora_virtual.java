d58d60f7368b4bbd690280b5a99f467aeeec8d7f
Carlos20daniel <1440648@senati.pe>
2023-08-21 12:48:15 -0500

    Sumar

--- Calculadora_virtual/src/main/java/com/mycompany/calculadora_virtual/Calculadora_virtual.java
+++ Calculadora_virtual/src/main/java/com/mycompany/calculadora_virtual/Calculadora_virtual.java
@@ -14,5 +14,14 @@ public class Calculadora_virtual {
 
     public static void main(String[] args) {
         Scanner entrada= new Scanner(System.in);
+        System.out.println("--------SUMA--------");
+        int a,b,suma;
+        System.out.println("Digite el primer numero:");
+        a=entrada.nextInt();
+        System.out.println("Digite el segundo numero:");
+        b=entrada.nextInt();
+        suma = a+b;
+        System.out.println("EL RESULTADO DE ESTA SUMA ES: " + suma);
+        
     }
 }
