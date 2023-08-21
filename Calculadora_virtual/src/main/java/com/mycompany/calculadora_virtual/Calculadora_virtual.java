560f65a5317990659f35019af1547cac22e78878
Carlos20daniel <1440648@senati.pe>
2023-08-21 12:52:03 -0500

    Restar

--- Calculadora_virtual/src/main/java/com/mycompany/calculadora_virtual/Calculadora_virtual.java
+++ Calculadora_virtual/src/main/java/com/mycompany/calculadora_virtual/Calculadora_virtual.java
@@ -14,5 +14,14 @@ public class Calculadora_virtual {
 
     public static void main(String[] args) {
         Scanner entrada= new Scanner(System.in);
+        System.out.println("--------RESTA--------");
+        int c,d,resta;
+        System.out.println("Digite el primer numero:");
+        c=entrada.nextInt();
+        System.out.println("Digite el segundo numero:");
+        d=entrada.nextInt();
+        resta = c-d;
+        System.out.println("EL RESULTADO DE ESTA RESTA ES: " + resta);
+        
     }
 }
