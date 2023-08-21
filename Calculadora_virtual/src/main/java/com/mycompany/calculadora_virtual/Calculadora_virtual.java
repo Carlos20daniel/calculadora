cbd24416939ea77b8e122e03077817212c1dd69a
Carlos20daniel <1440648@senati.pe>
2023-08-21 12:54:56 -0500

    Dividir

--- Calculadora_virtual/src/main/java/com/mycompany/calculadora_virtual/Calculadora_virtual.java
+++ Calculadora_virtual/src/main/java/com/mycompany/calculadora_virtual/Calculadora_virtual.java
@@ -14,5 +14,13 @@ public class Calculadora_virtual {
 
     public static void main(String[] args) {
         Scanner entrada= new Scanner(System.in);
+        System.out.println("--------DIVICION--------");
+        int e,f,divicion;
+        System.out.println("Digite el primer numero:");
+        e=entrada.nextInt();
+        System.out.println("Digite el segundo numero:");
+        f=entrada.nextInt();
+        divicion = e/f;
+        System.out.println("EL RESULTADO DE ESTA DIVICION ES:  " + divicion);
     }
 }
