d106c7af70f5e7a44be94831223c0f8535a0ec08
Carlos20daniel <1440648@senati.pe>
2023-08-21 12:57:06 -0500

    Multiplicar

--- Calculadora_virtual/src/main/java/com/mycompany/calculadora_virtual/Calculadora_virtual.java
+++ Calculadora_virtual/src/main/java/com/mycompany/calculadora_virtual/Calculadora_virtual.java
@@ -14,5 +14,13 @@ public class Calculadora_virtual {
 
     public static void main(String[] args) {
         Scanner entrada= new Scanner(System.in);
+        System.out.println("--------MULTIPLICACION--------");
+        int g,h,multiplicacion;
+        System.out.println("Digite el primer numero:");
+        g=entrada.nextInt();
+        System.out.println("Digite el segundo numero:");
+        h=entrada.nextInt();
+        multiplicacion = g*h;
+        System.out.println("EL RESULTADO DE ESTA MULTIPLICACION ES:  " + multiplicacion);
     }
 }
